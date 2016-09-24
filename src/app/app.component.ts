7708b399945d88e4e3f8467900c842decd08aae3
iguissouma <issam.guissouma@laposte.net>
2016-09-24 18:06:20 +0200

    fix(server): enable cors on apollo server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ import {client} from "./client";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app/app.component.html',
-  styleUrls: ['./app/app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 
 })
 export class AppComponent implements OnInit, AfterViewInit {
